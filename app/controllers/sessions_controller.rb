67ae000f04ff0970d070df9a929f2da961b7df3b
devanshimg <devanshi.gupta@kellogg.northwestern.edu>
2023-03-02 19:21:49 +0000

    auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,29 @@
 class SessionsController < ApplicationController
   def new
+
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # login the user
+        session["user_id"] = @user["id"]
+        redirect_to "/places"
+      else
+        redirect_to "/login"
+      end
+    else
+      redirect_to "/login"
+    end
   end
+  
 
   def destroy
+    flash["notice"] = "Goodbye!"
+    session["user_id"] = nil
+    redirect_to "/login"
   end
 end
   
